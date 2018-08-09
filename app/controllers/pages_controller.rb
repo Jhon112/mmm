fe46b705693619e0f2b5896aa165ffd80632b55c
Jhon Arias <jhonarmont@hotmail.com>
2018-08-08 20:57:00 -0500

    Creation of differents  views for About, Post and Contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+
+
+  #About us
+  def about
+    @name = "Jhon"
+  end
+
+
+  #contact
+  def contact
+    @address = "Calle 2B #81A - 380"
+    @email = "jhonarmont@hotmail.com"
+  end
+
+  # Posts 
+  def post
+    @arr = ["Este", "Es", "Un", "array"]
+  end
+
+
 end
