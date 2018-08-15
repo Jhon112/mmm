188ab33a54c768bfe94691287d84bcdd7bf6379a
Jhon Arias <jhonarmont@hotmail.com>
2018-08-15 18:37:15 -0500

    Created Products and all of its resources with rails g resource

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,18 @@ class Post < ApplicationRecord
     has_many :comments, as: :commentable
     # validates :category, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters" }  
     # validates :published, inclusion: { in: [true, false] }
+
+
+    scope :published, -> { where(published: true)}
+
+    before_create :fill_body, :mark_as_published
+
+
+    def fill_body
+        self.body = 'This is a Post'
+    end
+
+    def mark_as_published
+        published = true
+    end
 end
