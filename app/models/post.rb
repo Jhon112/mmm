91d7b28f66fdb2ace4868f01030b7e9b7b92f732
Jhon Arias <jhonarmont@hotmail.com>
2018-08-14 20:25:47 -0500

    Created model Comment with Polymorphic Association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     validates :title, :body, :published, presence: true
+    has_many :comments, as: :commentable
     # validates :category, format: {with: /\A[a-zA-Z]+\z/, message: "only allows letters" }  
     # validates :published, inclusion: { in: [true, false] }
 end
